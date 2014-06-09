76f5e0ba3cae2f660f72c878869dddd4e6213c34
Dell <Dell@Dell-PC>
2014-06-09 14:52:45 +0300

    - Add Test class, and implement testRotateLeft() method to test rotateLeft() method in Board class.

---
+++ test/Test.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import static org.junit.Assert.*;
+import game2048f.Board;
+
+/**
+ *
+ * @author Dell
+ */
+
+
+
+public class Test {
+
+    @org.junit.Test
+    public void testRotateLeft(){
+        Board board = new Board();
+        board.setCellValue(0, 3, 2);
+        board.rotateLeft();
+        assertEquals(board.getCellValue(0, 0), 2);
+        assertEquals(board.getCellValue(0, 1), 0);
+        assertEquals(board.getCellValue(0, 2), 0);
+        assertEquals(board.getCellValue(0, 3), 0);
+        
+    }
+    
+}
