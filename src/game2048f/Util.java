64f9474f83d0f3d5e9b9141dce88a6d9718ed0ef
Dell <Dell@Dell-PC>
2014-06-09 14:12:32 +0300

    - Add Util class, and add getRandomCellNumber() method.

---
+++ src/game2048f/Util.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package game2048f;
+
+/**
+ *
+ * @author Dell
+ */
+
+public class Util {
+    
+    public static int getRandomCellNumber(){
+        return (int)(Math.random() * 4);
+    }
+    
+}
